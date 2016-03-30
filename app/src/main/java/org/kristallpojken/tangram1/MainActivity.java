e14120bfdca7aed2df08e6fd8ddfb4178dc5312e
Iggy Drougge <idrougge@mac.com>
2016-03-30 15:04:47 +0200

    Lade till klass TileView

--- app/src/main/java/org/kristallpojken/tangram1/MainActivity.java
+++ app/src/main/java/org/kristallpojken/tangram1/MainActivity.java
@@ -3,6 +3,8 @@ package org.kristallpojken.tangram1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Main","Tile 5: "+Tiles.tiles[5]);
         int värden[]={1,2,3,4,5,0,5,4,3,2,1,0};
         PlayField fält=new PlayField(värden);
-        Log.i("Main","fält: \n"+fält);
+        //Log.i("Main","fält: \n"+fält);
+        TileView.context=this;              // Lägg in vår kontext i TileView-klassen
+        TileView urk=new TileView();
+        this.addContentView(urk,new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        /*
+        for (Tiles tile:fält.field)
+        {
+            ImageView tileView=new ImageView(this);
+            tileView.setImageDrawable(getDrawable(R.drawable.ruta180grd));
+            ViewGroup.LayoutParams lp=new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            this.addContentView(tileView,lp);
+        }
+        */
     }
 }
---
+++ app/src/main/java/org/kristallpojken/tangram1/TileView.java
@@ -0,0 +1,25 @@
+package org.kristallpojken.tangram1;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Klass för att rita en tangram-ruta på skärmen. Bygger på ImageView.
+ */
+public class TileView extends ImageView {
+    public static Context context;
+    private ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    TileView()
+    {
+        super(context, null);
+        this.setLayoutParams(lp);
+        this.setImageDrawable(context.getDrawable(R.drawable.ruta270grd));
+    }
+    TileView(Tiles tile)
+    {
+        this();
+        this.setImageDrawable(context.getDrawable(R.drawable.rutahel));
+    }
+}

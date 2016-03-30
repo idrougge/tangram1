6d01de78601c8facdba888eb48930454e3c19a63
Iggy Drougge <idrougge@mac.com>
2016-03-30 12:15:10 +0200

    Lade in unicodetecken i TileTypes.enum för enklare debug

--- app/src/main/java/org/kristallpojken/tangram1/PlayField.java
+++ app/src/main/java/org/kristallpojken/tangram1/PlayField.java
@@ -6,9 +6,20 @@ import android.util.Log;
  * Spelfältet är ett rutnät om åtm. 3 x 3 rutor
  */
 public class PlayField {
+    static final private int MINTILES=9;
+    TileTypes field[];
     PlayField(TileTypes[] tiles)
     {
         Log.i("PlayField","Skapar spelfält med "+tiles.length+" rutor.");
+        field=tiles.clone();
+    }
+    PlayField(int[] tiles)
+    {
+        if(tiles.length<MINTILES)
+        {
+            Log.e("PlayField","Fel vid init: Antalet rutor får inte vara mindre än "+MINTILES+"!");
+            return;
+        }
 
     }
 }
--- app/src/main/java/org/kristallpojken/tangram1/TileTypes.java
+++ app/src/main/java/org/kristallpojken/tangram1/TileTypes.java
@@ -4,18 +4,24 @@ package org.kristallpojken.tangram1;
  * Spelet har sex rutor: Tom, ifylld och halvifylld roterad i 90-graderssteg
  */
 public enum TileTypes {
-    _empty(0),_90deg(1),_180deg(2),_270deg(3),_360deg(4),_full(5);
+    _empty(0,"︎◻︎"),_90deg(1,"◥"),_180deg(2,"◢"),_270deg(3,"◣"),_360deg(4,"◤"),_full(5,"█");
     private int nr;
-    TileTypes(int nr)
+    private String text;
+    TileTypes(int nr,String text)
     {
         this.nr=nr;
+        this.text=text;
     }
     public int nr()
     {
         return nr;
     }
+    public String text()
+    {
+        return text;
+    }
     public String toString()
     {
-        return "Ruta med nr: "+nr;
+        return "Ruta med nr "+nr+": "+text;
     }
 }
